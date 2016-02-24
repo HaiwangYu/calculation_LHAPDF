8fc625562b5c1c173aac73b37b37a20a2b70dc6a
Haiwang Yu <yuhw.pku@gmail.com>
2016-02-24 11:40:28 -0500

    re-weighting code added

--- calc_asymmetry/testcode.cc
+++ calc_asymmetry/testcode.cc
@@ -18,6 +18,9 @@
 #include "TCanvas.h"
 #include "TGraphErrors.h"
 #include "TGraphAsymmErrors.h"
+#include "TLegend.h"
+
+#define LogDebug(message) std::cout << "DEBUG: "<< "  " << __FUNCTION__ << "  " << __LINE__ << " :::  " << message << std::endl
 
 using namespace LHAPDF;
 using namespace std;
@@ -31,6 +34,14 @@ const double G_Q2 = 10;
 const double G_CL_Scale = 2.;
 
 string G_binning_type = "pT";
+
+vector<TH1D*> G_v_A_LL_replicas;
+
+vector<double> G_v_weighting_factors;
+
+TGraphErrors *G_g_run13_data;
+
+
 
 double quadSum(double x, double y)
 {
@@ -48,6 +59,22 @@ double getMean(vector<double> v)
 	return mean/n;
 }
 
+double getMean(vector<double> v,  vector<double> vw)
+{
+	int n = v.size();
+	double mean = 0;
+	double norminal = 0;
+	//BOOST_FOREACH(double x, v)
+	for(int i=0;i<n;i++)
+	{
+		double x = v[i];
+		double w = vw[i];
+		mean += x*w;
+		norminal += w;
+	}
+	return mean/norminal;
+}
+
 double getStdDev(vector<double> v, double mean)
 {
 	int n = v.size();
@@ -58,6 +85,25 @@ double getStdDev(vector<double> v, double mean)
 	}
 
 	return sqrt(std_dev/(n-1));
+}
+
+double getStdDev(vector<double> v, double mean, vector<double> vw)
+{
+	int n = v.size();
+	double std_dev = 0;
+	double norminal = 0;
+	for(int i=0;i<n;i++)
+	{
+		double x = v[i];
+		double w = vw[i];
+
+		std_dev += (x-mean)*(x-mean)*w;
+		norminal += w;
+	}
+	norminal *= 1.*(n-1)/n;
+
+	//http://stats.stackexchange.com/questions/6534/how-do-i-calculate-a-weighted-standard-deviation-in-excel
+	return sqrt(std_dev/norminal);
 }
 
 #include "boost/tuple/tuple.hpp"
@@ -102,6 +148,7 @@ TGraphErrors* getDataPlot()
 	double y[] = {0.003,0.007,0.053};
 	double ex[] = {0,0,0};
 	double ey[] = {0.014,0.016,0.029};
+	//double ey[] = {0.005,0.005,0.005};
 	TGraphErrors*gr = new TGraphErrors(n,x,y,ex,ey);
 	return gr;
 }
@@ -112,28 +159,28 @@ void get_Bjorken_x_from_pT(double & x1, double & x2, double pT)
 	x2 = 0.0013 + 0.000063*pT;
 }
 
-TH1D *plotPDF(const string setname = "NNPDFpol11_100", const int imem = 0, double minlogx = -3, double maxlogx = 0)
-{
-	char *hname = Form("plotPDF_%s_%d_Q2=%2.1fGeV",setname.data(),imem,G_Q2);
-	const int nbin = 100;
-	const double dbin = (maxlogx-minlogx)/nbin;
-	TH1D *h_result = new TH1D(hname,hname,nbin,minlogx,maxlogx);
-	const PDF* pdf = mkPDF(setname, imem);
-	const int pid = 21;
-	for(double logx=minlogx;logx<maxlogx;logx+=dbin)
-	{
-		double x = pow(10,logx);
-		double xf = pdf->xfxQ2(pid, x, G_Q2);
-		h_result->Fill(logx+dbin/2.,xf);
-	}
-
-	delete pdf;
-
-	h_result->GetXaxis()->SetTitle("Log(x)");
-	h_result->GetYaxis()->SetTitle("xf(x)");
-
-	return h_result;
-}
+//TH1D *plotPDF(const string setname = "NNPDFpol11_100", const int imem = 0, double minlogx = -3, double maxlogx = 0)
+//{
+//	char *hname = Form("plotPDF_%s_%d_Q2=%2.1fGeV",setname.data(),imem,G_Q2);
+//	const int nbin = 100;
+//	const double dbin = (maxlogx-minlogx)/nbin;
+//	TH1D *h_result = new TH1D(hname,hname,nbin,minlogx,maxlogx);
+//	const PDF* pdf = mkPDF(setname, imem);
+//	const int pid = 21;
+//	for(double logx=minlogx;logx<maxlogx;logx+=dbin)
+//	{
+//		double x = pow(10,logx);
+//		double xf = pdf->xfxQ2(pid, x, G_Q2);
+//		h_result->Fill(logx+dbin/2.,xf);
+//	}
+//
+//	delete pdf;
+//
+//	h_result->GetXaxis()->SetTitle("Log(x)");
+//	h_result->GetYaxis()->SetTitle("xf(x)");
+//
+//	return h_result;
+//}
 
 TH2D *getAsymmetryEbE(
 		const int imem = 0,
@@ -265,45 +312,76 @@ TH1D *getAsymmetry(
 	return h_result;
 }
 
-vector<TH1D*> getAsymmetryUncertaintyVector(const int nmem = 100)
+vector<TH1D*> getAsymmetryReplicaVector(const int nmem = 100)
 {
-	vector<TH1D*> v_eigen;
+	vector<TH1D*> vrep;
 
 	for(int imem = 0;imem<=nmem;imem++)
 	{
 		TH1D* h_temp = getAsymmetry(imem);
-		v_eigen.push_back(h_temp);
+		vrep.push_back(h_temp);
 	}
 
-	return v_eigen;
+	return vrep;
 }
 
 
-vector<TH1D*> getAsymmetryEbEUncertaintyVector(
+vector<TH1D*> getAsymmetryEbEReplicaVector(
 		const int nmem = 100,
 		const string binning_type = "pT"
 		)
 {
-	vector<TH1D*> v_eigen;
+	vector<TH1D*> vrep;
 
-	for(int imem = 0;imem<=nmem;imem++)
+	for(int imem = 1;imem<=nmem;imem++)
 	{
 		TH1D* h_temp = getAsymmetryEbE(imem,binning_type)->ProfileX()->ProjectionX();
-		v_eigen.push_back(h_temp);
+		vrep.push_back(h_temp);
 	}
 
-	return v_eigen;
+	return vrep;
+}
+
+// if g_data is NULL, fill the vector with 1.
+vector<double> getWeithtingFactors(vector<TH1D*> v_hists, TGraphErrors* g_data)
+{
+	vector<double> vw;
+
+	//BOOST_FOREACH(TH1D* hist, v_hists)
+	for(int j=0;j<v_hists.size();j++)
+	{
+		TH1D* hist = v_hists[j];
+		double wf = 1;
+		if(g_data)
+		{
+			for(int i=0;i<3;i++)
+			{
+				double x, mean;
+				g_data->GetPoint(i,x,mean);
+				double error = g_data->GetErrorY(i);
+				double theory = hist->GetBinContent(hist->FindBin(x));
+
+				wf *= TMath::Gaus(theory,mean,error);
+				//DEBUG
+				cout<<Form("DEBUG: x=%f; \t mean=%f; \t error=%f; \t theory=%f; \t wf=%f; \n",x,mean,error,theory,wf);
+			}
+		}
+		//DEBUG
+		cout<<Form("DEBUG: wf=%f; \n",wf);
+		vw.push_back(wf);
+	}
+	return vw;
 }
 
 
 TGraphAsymmErrors* getAsymmetryEbEUncertaintyTGraphErrors(
-		const int nmem = 100,
 		const string uncertainty_method = "Counting",
 		const double nsigma = 1.,
-		TFile* f = NULL)
+		TFile* f = NULL,
+		const bool do_reweighting = false
+		)
 {
-	vector<TH1D*> v_eigen = getAsymmetryEbEUncertaintyVector(nmem,G_binning_type);
-	TH1D* h_template = v_eigen[0];
+	TH1D* h_template = getAsymmetryEbE(0,G_binning_type)->ProfileX()->ProjectionX();;
 
 	double xmin = h_template->GetXaxis()->GetXmin();
 	double xmax = h_template->GetXaxis()->GetXmax();
@@ -319,21 +397,30 @@ TGraphAsymmErrors* getAsymmetryEbEUncertaintyTGraphErrors(
 	for(int ibin=1;ibin<=nbin;ibin++)
 	{
 		vector<double> v_temporary;
-		for(int irep = 1; irep < v_eigen.size(); irep++)
+		BOOST_FOREACH(TH1D* h, G_v_A_LL_replicas)
 		{
-			TH1D* h = v_eigen[irep];
 			v_temporary.push_back(h->GetBinContent(ibin));
 		}
 		double mean = h_template->GetBinContent(ibin);
 		tge_x.push_back(h_template->GetXaxis()->GetBinCenter(ibin));
 		tge_exl.push_back(0);
 		tge_exh.push_back(0);
-		tge_y.push_back(mean);
 		if(uncertainty_method == "StdDeV")
 		{
-			double std_dev = getStdDev(v_temporary,mean);
+			double std_dev;
+			if(do_reweighting)
+			{
+				mean = getMean(v_temporary,G_v_weighting_factors);
+				std_dev = getStdDev(v_temporary,mean,G_v_weighting_factors);
+			}
+			else
+			{
+				mean = getMean(v_temporary);
+				std_dev = getStdDev(v_temporary,mean);
+			}
 			tge_eyl.push_back(nsigma*std_dev);
 			tge_eyh.push_back(nsigma*std_dev);
+			cout<<Form("DEBUG: ibin: %d; \t mean: %f; \t std_dev: %f; \n",ibin,mean,std_dev);
 		}
 		if(uncertainty_method == "Counting")
 		{
@@ -361,49 +448,13 @@ TGraphAsymmErrors* getAsymmetryEbEUncertaintyTGraphErrors(
 			}
 
 		}
+		tge_y.push_back(mean);
 	}
 
-	//TGraphErrors* tge = new TGraphErrors(tge_x.size(),&tge_x[0],&tge_y[0],&tge_ex[0],&tge_ey[0]);
 	TGraphAsymmErrors* tge = new TGraphAsymmErrors(tge_x.size(),
 			&tge_x[0],&tge_y[0],
 			&tge_exl[0],&tge_exh[0],
 			&tge_eyl[0],&tge_eyh[0]);
-
-	return tge;
-}
-
-TGraphErrors* getAsymmetryUncertaintyTGraphErrors(const int nmem = 100, const int nsigma = 2)
-{
-	vector<TH1D*> v_eigen = getAsymmetryUncertaintyVector(nmem);
-	TH1D* h_template = v_eigen[0];
-
-	double xmin = h_template->GetXaxis()->GetXmin();
-	double xmax = h_template->GetXaxis()->GetXmax();
-	int nbin = h_template->GetXaxis()->GetNbins();
-
-	vector<double> tge_x;
-	vector<double> tge_y;
-	vector<double> tge_ex;
-	vector<double> tge_ey;
-
-	for(int ibin=1;ibin<=nbin;ibin++)
-	{
-		vector<double> v_temporary;
-		for(int irep = 1; irep < v_eigen.size(); irep++)
-		{
-			TH1D* h = v_eigen[irep];
-			v_temporary.push_back(h->GetBinContent(ibin));
-		}
-		double mean = h_template->GetBinContent(ibin);
-		double std_dev = getStdDev(v_temporary,mean);
-
-		tge_x.push_back(h_template->GetXaxis()->GetBinCenter(ibin));
-		tge_ex.push_back(0);
-		tge_y.push_back(mean);
-		tge_ey.push_back(nsigma*std_dev);
-	}
-
-	TGraphErrors* tge = new TGraphErrors(tge_x.size(),&tge_x[0],&tge_y[0],&tge_ex[0],&tge_ey[0]);
 
 	return tge;
 }
@@ -447,110 +498,172 @@ TH1D* estimateOneValue(
 	return h_result;
 }
 
+void initStuff()
+{
+	// G_g_run13_data for the result
+	G_g_run13_data = getDataPlot();
+	G_g_run13_data->SetMarkerStyle(21);
+	G_g_run13_data->SetMarkerColor(kBlack);
+	G_g_run13_data->SetLineColor(kBlack);
+	if(G_binning_type == "pT")
+		G_g_run13_data->SetTitle(";p_{T} GeV; A_{LL}");
+	if(G_binning_type == "y")
+		G_g_run13_data->SetTitle(";|y|; A_{LL}");
+	if(G_binning_type == "None")
+		G_g_run13_data->SetTitle(";One Bin; A_{LL}");
+
+	// A_LL calculated by replicas
+	G_v_A_LL_replicas = getAsymmetryEbEReplicaVector(100,G_binning_type);//HARD_CODED
+
+	// 
+	G_v_weighting_factors = getWeithtingFactors(G_v_A_LL_replicas,G_g_run13_data);
+}
+
+//---------------------------------------------------------------------
+
+const double G_Min_Logx = -3;
+const double G_Max_Logx = -0;
+const int G_N_Points = 100;
+
+TH1D *plotPDF(
+		LHAPDF::PDF* pdf
+		)
+{
+	TH1D *h = new TH1D("h","h",G_N_Points,G_Min_Logx,G_Max_Logx);
+	double d_logx = (G_Max_Logx - G_Min_Logx)/G_N_Points;
+	for(double logx = G_Min_Logx+d_logx/2.; logx<G_Max_Logx; logx += d_logx)
+	{
+		double x = pow(10.,logx);
+		double xf = pdf->xfxQ2(21,x,G_Q2);
+		h->Fill(logx,xf);
+	}
+
+	return h;
+}
+
+TGraphErrors *getLogxXfErrorTH1D(
+		string pdf_name = "NNPDFpol11_100",
+		double n_scale = 1.,
+		const bool do_reweighting = false
+		)
+{
+	const LHAPDF::PDFSet set(pdf_name);
+
+	TH1D* h_central = plotPDF(set.mkPDF(0));
+
+	vector<TH1D*> v_pdf_dist_replicas;
+	for(int i=1;i<set.size();i++)
+	{
+		v_pdf_dist_replicas.push_back(plotPDF(set.mkPDF(i)));
+	}
+
+	double xmin = h_central->GetXaxis()->GetXmin();
+	double xmax = h_central->GetXaxis()->GetXmax();
+	int nbin = h_central->GetXaxis()->GetNbins();
+
+	vector<double> tge_x;
+	vector<double> tge_y;
+	vector<double> tge_xe;
+	vector<double> tge_ye;
+
+	for(int ibin=1;ibin<=nbin;ibin++)
+	{
+		vector<double> v_temporary;
+		BOOST_FOREACH(TH1D* h,v_pdf_dist_replicas)
+		{
+			v_temporary.push_back(h->GetBinContent(ibin));
+		}
+
+		double mean;
+		double std_dev;
+
+		if(do_reweighting)
+		{
+			mean = getMean(v_temporary,G_v_weighting_factors);
+			std_dev = getStdDev(v_temporary,mean,G_v_weighting_factors);
+		}
+		else
+		{
+			mean = getMean(v_temporary);
+			std_dev = getStdDev(v_temporary,mean);
+		}
+
+		tge_x.push_back(h_central->GetXaxis()->GetBinCenter(ibin));
+		tge_xe.push_back(0);
+		tge_y.push_back(mean);
+		tge_ye.push_back(n_scale*std_dev);
+	}
+
+	TGraphErrors* tge = NULL;
+
+	tge = new TGraphErrors(nbin,&tge_x[0],&tge_y[0],&tge_xe[0],&tge_ye[0]);
+	tge->SetName("tge_xf_logx_th1d");
+	tge->SetTitle(";log10(x);xf");
+	return tge;
+}
+
+//---------------------------------------------------------------------
+
 
 int main(int argc, char* argv[]) {
 	if (argc >= 2) {
 		G_binning_type = argv[1];
 	}
 
+	initStuff();
+
 	TFile *tfile = TFile::Open("./output/out.root","RECREATE");
 
-	// Test Code
-	//TH1D* h_test = plotPDF("NNPDF23_nlo_as_0124");
-	//TH1D* h_test = plotPDF("MSTW2008lo68cl_nf3");
-	//TH1D* h_test = plotPDF("NNPDF23_nlo_as_0119");
-	TH1D* h_test = plotPDF("NNPDF23_nlo_as_0119",0);
-	tfile->cd();
-	h_test->Write();
-
-	//
-	TCanvas *c_asymmetry_tge = new TCanvas("c_asymmetry_tge","c_asymmetry_tge",100,100,1000,800);
-
-	TGraphErrors *dataplot = getDataPlot();
-	dataplot->SetMarkerStyle(21);
-	dataplot->SetMarkerColor(kBlack);
-	dataplot->SetLineColor(kBlack);
-	if(G_binning_type == "pT")
-		dataplot->SetTitle(";p_{T} GeV; A_{LL}");
-	if(G_binning_type == "y")
-		dataplot->SetTitle(";|y|; A_{LL}");
-	if(G_binning_type == "None")
-		dataplot->SetTitle(";One Bin; A_{LL}");
-	dataplot->Draw("ap");
-
-	//TH1D* h_central = getAsymmetry();
-	//h_central->SetLineColor(41);
-	//h_central->SetLineWidth(5);
-	//h_central->GetYaxis()->SetRangeUser(G_YRANGEMIN,G_YRANGEMAX);
-	//h_central->Draw("same");
-
-	//TGraphErrors* g_uncertainty = getAsymmetryUncertaintyTGraphErrors();
-	//g_uncertainty->SetFillColor(41);
-	//g_uncertainty->SetFillStyle(3004);
-	//g_uncertainty->Draw("4");
-
 	TH1D* h_central_EbE = getAsymmetryEbE(0,G_binning_type)->ProfileX()->ProjectionX();
-	h_central_EbE->SetLineWidth(5);
-	h_central_EbE->SetLineColor(46);
-	h_central_EbE->GetYaxis()->SetRangeUser(G_YRANGEMIN,G_YRANGEMAX);
-	h_central_EbE->Draw("same");
-	tfile->cd();
-	h_central_EbE->SetName("h_central_EbE");
-	h_central_EbE->Write();
+	TGraphAsymmErrors* g_uncertainty_EbE = getAsymmetryEbEUncertaintyTGraphErrors("StdDeV",G_CL_Scale,tfile);
 
-	TGraphAsymmErrors* g_uncertainty_EbE = getAsymmetryEbEUncertaintyTGraphErrors(100,"Counting",G_CL_Scale,tfile);
-	g_uncertainty_EbE->SetFillColor(46);
-	g_uncertainty_EbE->SetFillStyle(3005);
-	g_uncertainty_EbE->Draw("4");
-	tfile->cd();
-	g_uncertainty_EbE->SetName("g_uncertainty_EbE");
-	g_uncertainty_EbE->Write();
+	TLegend* leg = NULL;
 
-	TGraphAsymmErrors* g_uncertainty_EbE_StdDev = getAsymmetryEbEUncertaintyTGraphErrors(100,"StdDeV",G_CL_Scale);
-	g_uncertainty_EbE_StdDev->SetFillColor(46);
-	g_uncertainty_EbE_StdDev->SetFillStyle(3005);
-	g_uncertainty_EbE_StdDev->Draw("4");
-	tfile->cd();
-	g_uncertainty_EbE_StdDev->SetName("g_uncertainty_EbE_StdDev");
-	g_uncertainty_EbE_StdDev->Write();
-
-	c_asymmetry_tge->Update();
-	tfile->cd();
-	c_asymmetry_tge->Write();
-	c_asymmetry_tge->SaveAs("compare.png");
-
-	if(true)// <pT> method result
+	// Main result
+	if(true)
 	{
-		//TCanvas *c_asymmetry_vector = new TCanvas("c_asymmetry_vector","c_asymmetry_vector",100,100,1000,800);
-		//h_central->Draw();
+		TCanvas *c_main = new TCanvas("c_main","c_main",100,100,1000,800);
+		G_g_run13_data->Draw("ap");
 
-		//vector<TH1D*> v_uncertainty = getAsymmetryUncertaintyVector();
-		//BOOST_FOREACH(TH1* h_temp, v_uncertainty)
-		//{
-		//	h_temp->SetLineColor(41);
-		//	h_temp->SetLineStyle(2);
-		//	h_temp->Draw("same");
-		//}
+		h_central_EbE->SetLineWidth(5);
+		h_central_EbE->SetLineColor(46);
+		h_central_EbE->GetYaxis()->SetRangeUser(G_YRANGEMIN,G_YRANGEMAX);
+		h_central_EbE->Draw("same");
+		h_central_EbE->SetName("h_central_EbE");
 
-		//g_uncertainty->SetFillColor(4);
-		//g_uncertainty->SetFillStyle(0);
-		//g_uncertainty->Draw("4");
+		g_uncertainty_EbE->SetFillColor(46);
+		g_uncertainty_EbE->SetFillStyle(3001);
+		g_uncertainty_EbE->Draw("4");
+		g_uncertainty_EbE->SetName("g_uncertainty_EbE");
 
-		//h_central->Draw("same");
-		//dataplot->Draw("samep");
-		//c_asymmetry_vector->Update();
+		TGraphAsymmErrors* g_uncertainty_EbE_rw = getAsymmetryEbEUncertaintyTGraphErrors("StdDeV",G_CL_Scale,tfile,true);
+		g_uncertainty_EbE_rw->SetFillColor(41);
+		g_uncertainty_EbE_rw->SetFillStyle(3004);
+		g_uncertainty_EbE_rw->Draw("p");
+		g_uncertainty_EbE_rw->SetName("g_uncertainty_EbE_rw");
 
-		//tfile->cd();
-		//c_asymmetry_vector->Write();
-		//c_asymmetry_vector->SaveAs("c_asymmetry_vector.png");
+		leg = new TLegend(0.1,0.6,0.5,0.9);
+		leg->AddEntry(h_central_EbE,"Central","l");
+		leg->AddEntry(g_uncertainty_EbE,"Uncertianty","f");
+		leg->AddEntry(g_uncertainty_EbE_rw,"Re-weighted","lep");
+		leg->Draw();
+
+
+		c_main->Update();
+		tfile->cd();
+		c_main->Write();
+		//c_main->SaveAs("compare.png");
+		h_central_EbE->Write();
+		g_uncertainty_EbE->Write();
+		g_uncertainty_EbE_rw->Write();
 	}
 
-	if(true)// Event-by-event plot all results
+	if(false)// show all replicas
 	{
-		TCanvas *c_asymmetry_EbE_vector = new TCanvas("c_asymmetry_EbE_vector","c_asymmetry_EbE_vector",100,100,1000,800);
+		TCanvas *c_show_all_replicas = new TCanvas("c_show_all_replicas","c_show_all_replicas",100,100,1000,800);
 		h_central_EbE->Draw();
 
-		vector<TH1D*> v_uncertainty_EbE = getAsymmetryEbEUncertaintyVector(100,G_binning_type);
+		vector<TH1D*> v_uncertainty_EbE = G_v_A_LL_replicas;
 		BOOST_FOREACH(TH1* h_temp, v_uncertainty_EbE)
 		{
 			h_temp->SetLineColor(46);
@@ -563,18 +676,51 @@ int main(int argc, char* argv[]) {
 		g_uncertainty_EbE->Draw("4");
 
 		h_central_EbE->Draw("same");
-		dataplot->Draw("samep");
-		c_asymmetry_EbE_vector->Update();
+		G_g_run13_data->Draw("samep");
+		c_show_all_replicas->Update();
 
 		tfile->cd();
-		c_asymmetry_EbE_vector->Write();
-		c_asymmetry_EbE_vector->SaveAs("c_asymmetry_EbE_vector.png");
+		c_show_all_replicas->Write();
+		//c_show_all_replicas->SaveAs("c_show_all_replicas.png");
 	}
 
+	// Test re-weighting
+	if(true)
+	{
+		TCanvas *c_rw = new TCanvas("c_rw","c_rw",100,100,1000,800);
+		TGraphErrors *g_pdf_og = getLogxXfErrorTH1D("NNPDFpol11_100",1.,NULL); // Origninal plot
+		g_pdf_og->SetMarkerStyle(20);
+		g_pdf_og->SetFillColor(41);
+		g_pdf_og->SetFillStyle(3001);
+		g_pdf_og->SetName("g_pdf_og");
+		g_pdf_og->Draw("ap4");
+
+		TGraphErrors *g_pdf_rw = getLogxXfErrorTH1D("NNPDFpol11_100",1.,G_g_run13_data);// Re-weighted plot
+		g_pdf_rw->SetMarkerStyle(20);
+		g_pdf_rw->SetFillColor(1);
+		g_pdf_rw->SetFillStyle(3005);
+		g_pdf_rw->SetName("g_pdf_rw");
+		g_pdf_rw->Draw("p");
+
+		TLegend* leg = new TLegend(0.5,0.1,0.9,0.3);
+		leg->AddEntry(g_pdf_og,"NNPDFpol1.1 origin","lf");
+		leg->AddEntry(g_pdf_rw,"NNPDFpol1.1 re-weighted with PHENIX data","lep");
+		leg->Draw();
+
+		c_rw->Update();
+
+		tfile->cd();
+		c_rw->Write();
+		g_pdf_og->Write();
+		g_pdf_rw->Write();
+
+	}
+
+
 	// One Value
-	TH1D* h_one_value = estimateOneValue();
-	tfile->cd();
-	h_one_value->Write();
+	//TH1D* h_one_value = estimateOneValue();
+	//tfile->cd();
+	//h_one_value->Write();
 
 	tfile->Close();
 
